2defb01a41868c69cd70d7ebf3addf2d939975b5
Michael Reames <michael_reames@yahoo.com>
2020-08-11 14:49:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import "./App.css";
+import { Redirect, Route,  Switch, useParams } from 'react-router-dom';
 import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
+import Contact from './components/Contact/Contact';
+import Navigation from './components/navigation/Navigation';
+import Error from './components/error/Error';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Grande Hey" />
-      <Clock />
+      <Navigation />
+      <Switch>
+      <Route 
+      exact strict
+      path="/"       
+      render={(props) =><Welcome {...props} name="Mike"/>} />
+      <Route 
+      exact
+      path="/welcome/:name" 
+      render={(props) => <Welcome {...props} name={props.match.params.name} />} />
+      
+      <Route exact path="/clock" component={Clock} />
+      <Route exact path="/contact" component={Contact} />
+      <Route component={Error} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/error/Error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Error (props) {
+return (
+    <div className="message">
+        Error 404: Sarcasm module not found
+    </div>
+)
+
+}
+export default Error
